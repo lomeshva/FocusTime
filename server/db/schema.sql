8df5adef6da8fc63a57df5bc4db632c30b3d6e22
Lomesh Vanapalli <133080381+lomeshva@users.noreply.github.com>
2025-11-18 20:30:51 -0600

    Delete server directory

--- server/db/schema.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE IF NOT EXISTS sessions(
-  session_id SERIAL PRIMARY KEY,
-  start_time TIMESTAMP,
-  end_time TIMESTAMP,
-  duration INT
-);
--- server/routes/sessionRoutes.js
+++
@@ -1,8 +0,0 @@
-const router=require('express').Router();
-const controller=require('../controllers/sessionController');
-
-router.post('/start', controller.startSession);
-router.post('/pause', controller.pauseSession);
-router.post('/stop', controller.stopSession);
-
-module.exports=router;
